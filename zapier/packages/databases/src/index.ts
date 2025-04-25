fce80296053c6b3d2bbd2758d9195f41d7d8c9c7
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2025-04-24 17:27:37 -0700

    refactor: remove unused files and configurations from the project

--- zapier/packages/databases/src/index.ts
+++
@@ -1,7 +0,0 @@
-// packages/database/src/index.ts
-import { PrismaClient } from '@prisma/client';
-
-export const prisma = new PrismaClient();
-
-// Export all types from Prisma client
-export * from '@prisma/client';
